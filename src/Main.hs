76512574b2f794867fba1700e9dc222dd29cd42d
OlleLinderos <olle@linderos.nu>
2019-12-28 14:08:47 +0100

    Use forM_ to do it n times

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,15 @@
 
 module Main where
 
-import           Network.HTTP.Simple     (httpBS, getResponseBody, Request, parseRequest_)
-import qualified Data.ByteString.Char8   as B8
-import           Control.Lens            ( preview )
-import           Data.Aeson.Lens         ( key, _String )
-import           Data.Text               ( Text )
-import qualified Data.Text.IO            as TIO
+import           Network.HTTP.Simple   ( httpBS, getResponseBody, Request, parseRequest_ )
+import qualified Data.ByteString.Char8 as B8
+import           Control.Monad
+import           Control.Lens          ( preview )
+import           Data.Aeson.Lens       ( key, _String )
+import           Data.Text             ( Text )
+import qualified Data.Text.IO          as TIO
 import           Data.List
+
 
 fetchIds :: IO B8.ByteString
 fetchIds = do
@@ -16,7 +18,7 @@ fetchIds = do
   return (getResponseBody res)
 
 parseIds :: B8.ByteString -> [B8.ByteString]
-parseIds x = B8.split ',' $ B8.tail $ B8.init x
+parseIds s = B8.split ',' $ B8.tail $ B8.init s
 
 getNthId :: [B8.ByteString] -> Int -> B8.ByteString
 getNthId x n = x!!n
@@ -25,8 +27,8 @@ byteStringToString :: B8.ByteString -> String
 byteStringToString x = filter (/='"') $ show x
 
 prepareUrl :: String -> Request
-prepareUrl x =
-  parseRequest_ $ intercalate "" ["https://hacker-news.firebaseio.com/v0/item/", x, ".json"] 
+prepareUrl id =
+  parseRequest_ $ intercalate "" ["https://hacker-news.firebaseio.com/v0/item/", id, ".json"] 
 
 fetchStory :: String -> IO B8.ByteString
 fetchStory id = do
@@ -39,17 +41,17 @@ getStoryTitle = preview (key "title" . _String)
 getStoryUrl :: B8.ByteString -> Maybe Text
 getStoryUrl = preview (key "url" . _String)
 
-joinStory t u = intercalate " - " [t, u]
-
 main :: IO ()
 main = do
   ids   <- fetchIds
-  story <- fetchStory $ byteStringToString $ getNthId (parseIds ids) 0
+  forM_ [1..10] $ \n -> do
+         story <- fetchStory $ byteStringToString $ getNthId (parseIds ids) n
 
-  case getStoryTitle story of
-    Nothing    -> TIO.putStrLn "Nothing"
-    Just title -> TIO.putStrLn $ title
+         case getStoryTitle story of
+           Nothing    -> TIO.putStrLn "Nothing"
+           Just title -> TIO.putStrLn $ title
 
-  case getStoryUrl story of
-    Nothing    -> TIO.putStrLn "Nothing"
-    Just url   -> TIO.putStrLn $ url
+         case getStoryUrl story of
+           Nothing    -> TIO.putStrLn "Nothing"
+           Just url   -> TIO.putStrLn $ url
+
