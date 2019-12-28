40f6a1c6de13e63686527679770c19f873d96991
OlleLinderos <olle@linderos.nu>
2019-12-28 16:30:33 +0100

    Add ability to navigate with "n" and "p"

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,15 @@ module Main where
 
 import           Network.HTTP.Simple   ( httpBS, getResponseBody, Request, parseRequest_ )
 import qualified Data.ByteString.Char8 as B8
-import           Control.Monad
+import           Control.Monad         ( forM_, when )
 import           Control.Lens          ( preview )
 import           Data.Aeson.Lens       ( key, _String )
 import           Data.Text             ( Text )
 import qualified Data.Text.IO          as TIO
 import           Data.List
+import           System.Console.ANSI
+import           System.IO             (stdin, hReady, hSetEcho, hSetBuffering, BufferMode(NoBuffering))
+
 
 
 fetchIds :: IO B8.ByteString
@@ -28,7 +31,8 @@ byteStringToString x = filter (/='"') $ show x
 
 prepareUrl :: String -> Request
 prepareUrl id =
-  parseRequest_ $ intercalate "" ["https://hacker-news.firebaseio.com/v0/item/", id, ".json"] 
+  parseRequest_ $ intercalate ""
+  ["https://hacker-news.firebaseio.com/v0/item/", id, ".json"] 
 
 fetchStory :: String -> IO B8.ByteString
 fetchStory id = do
@@ -41,17 +45,51 @@ getStoryTitle = preview (key "title" . _String)
 getStoryUrl :: B8.ByteString -> Maybe Text
 getStoryUrl = preview (key "url" . _String)
 
+printStories :: [Int] -> B8.ByteString -> IO ()
+printStories r ids = do
+  forM_ r $ \n -> do
+    let i = show (n+1)
+    story <- fetchStory
+             $ byteStringToString
+             $ getNthId (parseIds ids) n
+
+    putStr $ i ++ ". " 
+
+    case getStoryTitle story of
+      Nothing    -> TIO.putStrLn "Nothing"
+      Just title -> TIO.putStrLn $ title
+
+    case getStoryUrl story of
+      Nothing    -> TIO.putStrLn "Nothing"
+      Just url   -> TIO.putStrLn $ url
+
+    putStr "\n"
+
+getKey :: IO [Char]
+getKey = reverse <$> getKey' ""
+  where getKey' chars = do
+          char <- getChar
+          more <- hReady stdin
+          (if more then getKey' else return) (char:chars)
+
+actions n ids = do 
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  key <- getKey
+  when (key /= "ESC") $ do
+    case key of
+      "n" -> do
+        clearScreen
+        printStories (map (\x -> x + 8) n) ids
+        actions (map (\x -> x + 8) n) ids
+      "p" -> do
+        clearScreen
+        printStories (map (\x -> x - 8) n) ids
+        actions (map (\x -> x - 8) n) ids
+      _ -> return ()
+  
 main :: IO ()
 main = do
-  ids   <- fetchIds
-  forM_ [1..10] $ \n -> do
-         story <- fetchStory $ byteStringToString $ getNthId (parseIds ids) n
-
-         case getStoryTitle story of
-           Nothing    -> TIO.putStrLn "Nothing"
-           Just title -> TIO.putStrLn $ title
-
-         case getStoryUrl story of
-           Nothing    -> TIO.putStrLn "Nothing"
-           Just url   -> TIO.putStrLn $ url
-
+  ids <- fetchIds
+  printStories [0..7] ids
+  actions [0..7] ids
